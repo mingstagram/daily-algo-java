f16ac2c3192551755350cd42c5382613f99e5af6
mingstagram <hikariguitar1@gmail.com>
2025-04-09 13:46:33 +0900

    ✅ 2025-04-08 - BOJ 9012 괄호 ✅ 2025-04-09 - BOJ 10828 스택

--- src/day01_2025_04_08/Main.java
+++ src/day01_2025_04_08/Main.java
@@ -3,9 +3,30 @@ package day01_2025_04_08;
 import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
 
+        for (int i = 0; i < T; i++) {
+            String str = sc.nextLine();
+            Stack<Character> stack = new Stack<>();
+            boolean valid = true;
+
+            for (char ch : str.toCharArray()) {
+                if (ch == '(') {
+                    stack.push(ch);
+                } else {
+                    if (stack.isEmpty()) {
+                        valid = false;
+                        break;
+                    }
+                    stack.pop();
+                }
+            }
+
+            if (!stack.isEmpty()) valid = false;
+            System.out.println(valid ? "YES" : "NO");
+        }
     }
-
 }
--- src/day02_2025_04_09/Main.java
+++ src/day02_2025_04_09/Main.java
@@ -1,4 +1,60 @@
 package day02_2025_04_09;
 
+import java.util.*;
+
 public class Main {
+
+    static Stack<Integer> stack = new Stack<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        for(int i = 0; i < N; i++) {
+            String command = sc.next();
+            if(command.equals("push")) {
+                int num = sc.nextInt();
+                push(num);
+            } else {
+                switch (command) {
+                    case "pop":
+                        System.out.println(pop());
+                        break;
+                    case "size":
+                        System.out.println(size());
+                        break;
+                    case "empty":
+                        System.out.println(empty());
+                        break;
+                    case "top":
+                        System.out.println(top());
+                        break;
+                }
+            }
+        }
+
+    }
+
+    static void push(int x) {
+        stack.push(x);
+    }
+
+    static int pop() {
+        if(stack.isEmpty()) return -1;
+        else return stack.pop();
+    }
+
+    static int size() {
+        return stack.size();
+    }
+
+    static int empty() {
+        if(stack.isEmpty()) return 1;
+        else return 0;
+    }
+
+    static int top() {
+        if(stack.isEmpty()) return -1;
+        else return stack.peek();
+    }
+
 }
